f1ca6405c86c5b281d189d28c2bbb6718fff7eb4
Emily Asch <e.asch@me.com>
2018-07-21 17:20:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout (string) {
+console.log(hello.toUpperCase());
+}
+
+function logWhisper(string){
+ console.log("HELLO".toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  if (sayHiToGrandma ==="hello"){
+    console.log("I can't hear you")
+  } else if (sayHiToGrandma === "HELLO"){
+    console.log("YES INDEED!")
+  }else {
+    console.log("I love you too")
+  }
+    
+  }
+
+
+
